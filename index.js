e79c7e6d036f105aca38021a26a6185774b29425
Rukhsat <rhjim2005@gmail.com>
2025-11-27 23:56:38 +0600

    Connected MongoDB

--- index.js
+++ index.js
@@ -2,11 +2,41 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000;
-app.use(cors())
+const dotenv = require('dotenv')
+dotenv.config()
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://FinEase:${process.env.MongoDb_pass}@cluster0.we4ne2s.mongodb.net/?appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.use(cors())
+app.use(express.json())
+/*
+FinEase
+*/
 app.get('/', (req, res) => {
   res.send('FinEase Server is running')
 })
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`FinEase Server listening on port ${port}`)
